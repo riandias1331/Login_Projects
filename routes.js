b67534ce4b5b8dd6a6a23626a121a2059fec9f55
riandias1331 <pereirarian@gmail.com>
2025-05-25 21:08:20 -0300

    Feat: Add jwt

--- routes.js
+++ routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const Controller = require("./src/controllers/Controller.js")
+const authMiddleware = require("./src/middlwares/auth.js");
 
 // private routes
-router.get("/", Controller.getUsers);
+router.get("/users", authMiddleware, Controller.getUsers);
+router.get("/users/:id", authMiddleware, Controller.getUserById);
+router.put("/users/:id", authMiddleware, Controller.updateUser);
+router.delete("/users/:id", authMiddleware, Controller.deleteUser);
+router.delete("/users", authMiddleware, Controller.deleteAllUsers);
 
 // public routes
 router.post("/register", Controller.register);
--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -2,6 +2,8 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const bcrypt = require("bcrypt");
+const jwt= require("jsonwebtoken");
+const jwt_secret = process.env.JWT_SECRET;
 
 exports.getUsers = async (req, res) => {
   try {
@@ -12,6 +14,90 @@ exports.getUsers = async (req, res) => {
   }
 };
 
+exports.getUserById = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: 'User does not exist' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+exports.updateUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { name, email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: 'User does not exist' });
+    }
+
+    const updatedData = {
+      name,
+      email,
+    };
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      updatedData.password = await bcrypt.hash(password, salt);
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: updatedData,
+    });
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: 'User does not exist' });
+    }
+
+    await prisma.user.delete({
+      where: { id: userId },
+    });
+
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+exports.deleteAllUsers = async (req, res) => {
+  try {
+    await prisma.user.deleteMany();
+    res.status(200).json({ message: 'All users deleted successfully' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+// Public Routes
 exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -35,8 +121,10 @@ exports.register = async (req, res) => {
       },
     });
 
-    console.log('User created successfully:', user);
-    res.status(201).json({ message: 'User created successfully'});
+    const token = jwt.sign({ id: user.id }, jwt_secret, { expiresIn: "1h" });
+
+    console.log('Token: ', token);
+    res.status(201).json({ message: 'User created successfully', token, user });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -62,6 +150,7 @@ exports.login = async (req, res) => {
       return res.status(400).json({ error: 'Invalid Password' });
     }
 
+    
     console.log('Login successful:', user);
     res.status(200).json({ message: 'Login successful' });
   } catch (error) {
